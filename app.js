d183294e012dfd154ba57f451bfd4f0079e2036e
Patrick Lombardo <LombardoP@CalSAWS.org>
2020-10-08 21:06:23 -0700

    add controllers

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("Hello");
+//Storage Controller
+
+// Item Controller
+const ItemCtrl = (function () {
+  // Item Constructor
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+
+  // Data Structure (State)
+  const data = {
+    items: [
+      { id: 0, name: "Steak Dinner", calories: 1200 },
+      { id: 0, name: "Cookie", calories: 400 },
+      { id: 0, name: "Eggs", calories: 300 },
+    ],
+    currentItem: null,
+    totalCalories: 0,
+  };
+
+  return {
+    logData: function () {
+      return data;
+    },
+  };
+})();
+
+// UI Controller
+const UICtrl = (function () {
+  // Public Methods
+  return {};
+})();
+
+// App Controller
+const App = (function (ItemCtrl, UICtrl) {
+  // Public Methods
+  return {
+    init: function () {
+      console.log("Initializing App...");
+    },
+  };
+})(ItemCtrl, UICtrl);
+
+App.init();
